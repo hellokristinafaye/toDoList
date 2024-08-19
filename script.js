4d00ea327f949a40fa02fc985e59c0cf72d9bebc
Kristina Faye <hellokristinafaye@gmail.com>
2024-08-18 19:14:04 -0700

    addTask function added

--- script.js
+++ script.js
@@ -3,3 +3,13 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container"); 
 
+function addTask() {
+    if (inputBox.value === "") {
+        alert("You must write something!");
+    }
+    else {
+        let li = documment.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+    }
+}
