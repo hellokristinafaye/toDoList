ab8b35a59931e3ec3ac72fba0733ca5db6a1b9b6
Kristina Faye <hellokristinafaye@gmail.com>
2024-08-18 19:10:40 -0700

    initial JS coding- adding variables and an onclick

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-console.log("Hello There!");
+// console.log("Hello There!");
+
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container"); 
 
