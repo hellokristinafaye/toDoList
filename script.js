4faf1334ca1595110050df854d3b70f74fa65ead
Kristina Faye <hellokristinafaye@gmail.com>
2024-08-18 21:04:00 -0700

    save to local storeage code added and called when appropriate

--- script.js
+++ script.js
@@ -16,13 +16,20 @@ function addTask() {
         li.appendChild(span);
     }
     inputBox.value = '';
+    saveData()
 }
 
 listContainer.addEventListener("click", function(e) {
     if (e.target.tagName === "LI") { 
         e.target.classList.toggle("checked");
+        saveData()
     }
     else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
+        saveData()
     }
 }, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
